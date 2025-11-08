e54ec1a4887d742d1d15ea42606b1ed4f7d1bcd6
h <haifa9493@gmail.com>
2025-11-08 19:27:43 +0300

    Update 6-final-user.js implementation

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,14 +1,13 @@
 import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
-export default function handleProfileSignup(firstName, lastName, fileName) {
-  const promises = [
-    signUpUser(firstName, lastName),
-    uploadPhoto(fileName),
-  ];
-
-  return Promise.allSettled(promises).then((results) => results.map((result) => ({
-    status: result.status,
-    value: result.status === 'fulfilled' ? result.value : result.reason,
-  })));
+export default async function handleProfileSignup(firstName, lastName, fileName) {
+  const result = [];
+  await signUpUser(firstName, lastName)
+    .then((value) => { result.push({ status: 'fulfilled', value }); })
+    .catch(() => result.push({ status: 'rejected' }));
+  await uploadPhoto(fileName)
+    .then((value) => { result.push({ status: 'fulfilled', value }); })
+    .catch((error) => result.push({ status: 'rejected', value: error.toString() }));
+  return result;
 }
